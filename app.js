66034fc78e70deb284f3c698b8b855da78b1e396
ANOOP <connect.anoopjaiswal@gmail.com>
2024-06-06 21:42:34 +0530

    Adding jsx to project

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", abc: "h1-tag" },
-//   "heading"
-// );
-// //above object inside createEelememt is use to write attributes
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// console.log(heading); //it will return an object
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// const div1 = React.createElement("div", { id: "div", key: "1" }, [
+//   React.createElement("div", { id: "div2", key: "2" }, [
+//     React.createElement("h1", { id: "h1", key: "3" }, "i am child1 h1"),
+//     React.createElement("h2", { id: "h2", key: "4" }, "i am child1 h2"),
+//   ]),
+//   React.createElement("div", { id: "div2", key: "5" }, [
+//     React.createElement("h1", { id: "h1", key: "6" }, "i am child2  h1"),
+//     React.createElement("h2", { id: "h2", key: "7" }, "i am child2 h2"),
+//   ]),
+// ]);
+// const heading = React.createElement("h1", { id: "heading" }, "this is h1 tag");
 
 //
-// const heading = React.createElement("h1", { id: "heading" }, "this is h1 tag");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-const div1 = React.createElement("div", { id: "div" }, [
-  React.createElement("child", { id: "div2" }, [
-    React.createElement("h1", { id: "h1" }, "i am child1 h1"),
-    React.createElement("h2", { id: "h2" }, "i am child1 h2"),
-  ]),
-  React.createElement("child2", { id: "div2" }, [
-    React.createElement("h1", { id: "h1" }, "i am child2 h1"),
-    React.createElement("h2", { id: "h2" }, "i am child2 h2"),
-  ]),
-]);
-const heading = React.createElement("h1", { id: "heading" }, "this is h1 tag");
+// JSX -> Transpiled before it reached to JS ->PARCEL => Babel
+// JSX =>React.createElement => ReactElemet => JS object => HTML element => render
+const jsxHeading = <h1>Testing Jsx heading</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div1);
+root.render(jsxHeading);
