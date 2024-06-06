053a1434b0dc55d230b9de9d72398e0e740f03de
ANOOP <connect.anoopjaiswal@gmail.com>
2024-06-06 23:07:21 +0530

    react functional component and component composition

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX -> Transpiled before it reached to JS ->PARCEL => Babel
-// JSX =>Babel transpile it into React.createElement => ReactElemet => JS object => HTML element => render
-const jsxHeading = <h1>Testing Jsx heading</h1>;
+
+const Title = () => {
+  return <>Rendering Title</>;
+};
+
+const Heading = () => {
+  return (
+    <>
+      <Title />
+      <h1>Rendering Heading</h1>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Heading />);
